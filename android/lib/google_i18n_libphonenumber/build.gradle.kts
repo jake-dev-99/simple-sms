2c5a8b46444604058d98f6f5a7bc58fa574b1dd9
jakedev99 <6440039+jake-dev-99@users.noreply.github.com>
2026-04-20 21:46:02 -0400

    Bumped Android Build Versions

--- android/lib/google_i18n_libphonenumber/build.gradle.kts
+++ android/lib/google_i18n_libphonenumber/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.google.i18n.phonenumbers"
-    compileSdk = 35  // Updated to 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 30 // Default minimum SDK
@@ -40,11 +40,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "21"
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 
     publishing {
